5a27c240ecc3f4d9d02034eddbe77e83d5aab2ca
unknown <mbuscemi1999@gmail.com>
2022-05-08 20:19:40 +0200

    feature: homepage.dart: added basic layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:hexcolor/hexcolor.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
@@ -17,32 +20,34 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final style =  const TextStyle(fontSize: 62, fontWeight: FontWeight.bold);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: HexColor('#313b73'),
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(60.0),
-        child: AppBar(
-          automaticallyImplyLeading: false,
+        extendBodyBehindAppBar: true,
+        backgroundColor: HexColor('#313b73'),
+        appBar: AppBar(
           backgroundColor: Colors.transparent,
           elevation: 0.0,
-          title: Text(''),
+          title: const Text(''),
           centerTitle: true,
           leading: Padding(
-              padding: EdgeInsets.only(left: 16.0, top: 7.0, bottom: 7.0),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(8.0),
-                child: SvgPicture.asset('assets/images/twixter_logo.svg',color: HexColor('#E4EAF5'),),
-              )),
+            padding: const EdgeInsets.only(top: 7.0, left: 8),
+            child: SvgPicture.asset(
+              'assets/images/twixter_logo.svg',
+              color: HexColor('#E4EAF5'),
+              
+            ),
+          ),
           actions: [
             IconButton(
-              
-              icon: Image.asset('assets/images/twixter_dm.png'),
-              onPressed: () {
-                Navigator.of(context).pop();
-              }),
+                padding: const EdgeInsets.only(top: 7, right: 5, left: 8),
+                icon: Image.asset('assets/images/twixter_dm.png'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                }),
             IconButton(
+              padding: const EdgeInsets.all(6),
               icon: Image.asset('assets/images/twixter_profile.png'),
               onPressed: () {
                 Navigator.of(context).pop();
@@ -50,7 +55,36 @@ class _HomePageState extends State<HomePage> {
             ),
           ],
         ),
-      ),
-    );
+        body: Stack(children: [
+          Container(
+            height: 400,
+            width: double.infinity,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage('assets/images/homepage_bg1.png'),
+                  fit: BoxFit.cover),
+            ),
+            child: BackdropFilter(
+                filter: ImageFilter.blur(sigmaX: 2.5, sigmaY: 2.5),
+                child: Container(
+                  decoration:
+                      BoxDecoration(color: Colors.white.withOpacity(0.0)),
+                )), // Foreground widget here
+          ),
+          Container(
+            height: double.infinity,
+            decoration: BoxDecoration(
+                color: Colors.white,
+                gradient: LinearGradient(
+                    begin: FractionalOffset.topCenter,
+                    end: FractionalOffset.bottomCenter,
+                    colors: [
+                      HexColor('#575EA6').withOpacity(0.5),
+                    ],
+                    stops: const [
+                      1.0
+                    ])),
+          ), Text('Hello', style: GoogleFonts.poppins(textStyle: style)),
+        ]));
   }
 }
