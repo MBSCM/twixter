4fd5f37ff56d07c59a18172cdbb8eb31d5f06540
unknown <mbuscemi1999@gmail.com>
2022-05-18 15:25:30 +0200

    feature: login_page.dart: start login page

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:hexcolor/hexcolor.dart';
 
 class LoginPage extends StatefulWidget {
@@ -19,25 +20,36 @@ class _LoginPageState extends State<LoginPage> {
         elevation: 0.0,
         title: const Text(''),
         centerTitle: true,
-        leading: IconButton(
-          padding: const EdgeInsets.all(10),
-          icon: Image.asset(
-            'assets/images/twixter_back.png',
-          ),
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-        ),
+        leading: const Text(''),
+        actions: [
+          IconButton(
+              padding: const EdgeInsets.only(top: 7, right: 5, left: 8),
+              icon: Image.asset('assets/images/twixter_close.png'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              }),
+        ],
       ),
       body: Stack(children: [
         Align(
-          alignment: Alignment.topCenter,
-          child: Container(
-            padding: const EdgeInsets.only(top: 20),
-            child: SvgPicture.asset(
-          'assets/images/twixter_logo.svg',
-          color: HexColor('#E4EAF5'),height: 75,
-          ))),
+            alignment: Alignment.topCenter,
+            child: Container(
+                padding: const EdgeInsets.only(top: 20),
+                child: SvgPicture.asset(
+                  'assets/images/twixter_logo.svg',
+                  color: HexColor('#E4EAF5'),
+                  height: 150,
+                ))),
+        Center(
+          child: Padding(
+            padding: const EdgeInsets.only(bottom: 220),
+            child: Text('Meld je aan om gebruik the maken \nvan Twixter!',
+                style: GoogleFonts.poppins(
+                    textStyle:
+                        TextStyle(color: HexColor('#E4EAF5'), fontSize: 18),
+                    fontWeight: FontWeight.w700)),
+          ),
+        ),
       ]),
     );
   }
